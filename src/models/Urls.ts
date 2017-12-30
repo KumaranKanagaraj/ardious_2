741d228395814c6505ef36534eb5bcc773bca50f
ROOT\kumaran.kanagaraj <kumaran.kanagaraj@syncfusion.com>
2017-12-30 22:32:55 +0530

    Haved included the missing files

---
+++ src/models/Urls.ts
@@ -0,0 +1,5 @@
+export class Urls {
+    public static staticProperty = "Test";
+    private static corsUrl = 'https://cors-anywhere.herokuapp.com'
+    private static getQuestionsURL = 'http://kayamai.thinkbyfun.com/api/example/user'
+}
---
+++ src/providers/kayamai/kayamai.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { Urls } from '../../models/models';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the KayamaiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class KayamaiProvider {
+
+  // private corsUrl = 'https://cors-anywhere.herokuapp.com'
+  // private getQuestionsURL = 'http://kayamai.thinkbyfun.com/api/example/user'
+
+  constructor(public http: Http) {
+    console.log('Hello KayamaiProvider Provider');   //TODO: Remove Console
+  }
+
+  private constructHeaders() : RequestOptions{
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    headers.append('Accept', 'application/json');
+    headers.append('Authorization', 'Basic YWRtaW46MTIzNA==');
+    headers.append('X-API-KEY', 'CODEX@123');
+    return new RequestOptions({ headers: headers });
+  }
+
+  private fetchJSON(url:string, options:RequestOptions): Promise<any> {
+    return this.http
+    .get(url, options)
+    .map((response) => {
+     console.log(" From fetchJSON : "+response.json());   //TODO: Remove Console
+      return response.json()
+    })
+    .toPromise();
+  }
+
+  public getQuestions(): any {
+    return this.fetchJSON(Urls.corsUrl + "/" + Urls.getQuestionsURL,this.constructHeaders());
+  }
+
+
+}
